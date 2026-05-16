b4b1c4cc3970ad83396fd91582949b2d3eafbd56
emung <emanuel.ungheanu@hotmail.com>
2026-05-16 16:04:07 +0200

    Update constants.ts

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://192.168.0.101:3100/api/v1';
+export const API_BASE_URL = 'http://192.168.0.100:3100/api/v1';
 export const EXPENSES_URL = `${API_BASE_URL}/expenses`;
